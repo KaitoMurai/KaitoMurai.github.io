90b72a4f43c78ae54aeb88d125e64e017a677121
KaitoMurai <32830771+KaitoMurai@users.noreply.github.com>
2017-10-18 14:13:30 +0900

    Update javascript.js

--- web/javascript.js
+++ web/javascript.js
@@ -1,21 +1,19 @@
-function DspClk() {
-    DD = new Date();
-    Year = DD.getYear() + 1900;
-    Month = DD.getMonth() + 1;
-    Day = DD.getDate();
-
-    Hours = DD.getHours();
-    Minutes = DD.getMinutes();
-    Seconds = DD.getSeconds();
-
-    dayOfWeek = DD.getDay();	// 曜日(数値)
-    dayOfWeekStr = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];	// 曜日(日本語表記)
-    dayName = dayOfWeekStr[dayOfWeek];
-
-    document.getElementById("time").innerHTML = Year + "/" + ("0" + Month).slice(-2) + "/" + ("0" + Day).slice(-2) + "(" + dayName + ")" + " " + ("0" + Hours).slice(-2) + ":" + ("0" + Minutes).slice(-2) + ":" + ("0" + Seconds).slice(-2);
-
-    document.getElementById(Seconds % 10).style.backgroundColor = "salmon";
-    for (i = 0; i < 10; i++) {
-        if (!(Seconds % 10 == i)) document.getElementById(i).style.backgroundColor = "white";
-    }
+function set2fig(num) {
+    var ret;
+    if (num < 10) { ret = "0" + num; }
+    else { ret = num; }
+    return ret;
 }
+function showClock2() {
+    var nowTime = new Date();
+    var thisMonth = set2fig(nowTime.getMonth()+1);
+    var today = set2fig(nowTime.getDate());
+    var nowHour = set2fig(nowTime.getHours());
+    var nowMin = set2fig(nowTime.getMinutes());
+    var nowSec = set2fig(nowTime.getSeconds());
+    var dayOfWeek = nowTime.getDay();
+    var dayOfWeekStr = ["(Sun) ", "(Mon) ", "(Tue) ", "(Wed) ", "(Thu) ", "(Fri) ", "(Sat) "][dayOfWeek];
+    var msg = "現在時刻は" + thisMonth + "/" + today + " " + dayOfWeekStr + nowHour + "：" + nowMin + "：" + nowSec + "です";
+    document.getElementById("RealtimeClockArea2").innerHTML = msg;
+}
+setInterval('showClock2()', 100);
