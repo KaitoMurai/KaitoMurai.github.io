25a1f58548f8ddf734ee85bbb8233a08b7af7a90
KaitoMurai <32830771+KaitoMurai@users.noreply.github.com>
2019-06-12 23:08:08 +0900

    Update train.js

--- portal/train.js
+++ portal/train.js
@@ -1,1 +1,11 @@
-
+$(function(){
+  $.ajax({
+    url: 'https://transit.yahoo.co.jp/traininfo/detail/613/0/',    // 表示させたいコンテンツがあるページURL
+    cache: false,
+    datatype: 'html',
+    success: function(html) {
+      var h = $(html).find('#hoge');    // 表示させたいコンテンツの要素を指定
+      $('#wrap').append(h);    // append関数で指定先の要素へ出力
+    }
+  });
+});
